935758230b1d6d414be09f2ef365ded7f308dc6d
Glitch (sfold) <none>
2020-01-23 13:23:01 +0000

    add saver, finish it...

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+const box = document.getElementById("wrap");
+box.onclick = () => {
+  const oldB = box.style.border;
+  box.style.border = "none";
+  html2canvas(box, { backgroundColor: "#000000" }).then(canvas => {
+    document.body.appendChild(canvas);
+    box.style.border = oldB;
+    var link = document.getElementById("link");
+    link.setAttribute("download", "Fold.png");
+    link.setAttribute(
+      "href",
+      canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
+    );
+    link.click();
+    document.querySelector("canvas").remove();
+  });
+};
